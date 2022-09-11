b42abb8b0163b32b132204ef4e8d3afa3f2bad05
Mojito <vu3xk41997@gmail.com>
2022-09-12 01:10:15 +0200

    add props and local storage

--- src/Employees.js
+++ src/Employees.js
@@ -1,112 +1,9 @@
 // Hook
-import {useState} from "react";
 import femaleProfile from "./images/femaleProfile.jpeg";
 import maleProfile from "./images/maleProfile.jpeg";
 
-const Employees = () => {
-
-    const [selectedTeam, setTeam] = useState("TeamB");
-
-    const [employees, setEmployees] = useState(
-        [
-            {
-                id: 1,
-                fullName: "Bob Jones",
-                designation: "JavaScript Developer",
-                gender: "male",
-                teamName: "TeamA"
-            },
-            {
-                id: 2,
-                fullName: "Jill Bailey",
-                designation: "Node Developer",
-                gender: "female",
-                teamName: "TeamA"
-            },
-            {
-                id: 3,
-                fullName: "Gail Shepherd",
-                designation: "Java Developer",
-                gender: "female",
-                teamName: "TeamA"
-            },
-            {
-                id: 4,
-                fullName: "Sam Reynolds",
-                designation: "React Developer",
-                gender: "male",
-                teamName: "TeamB"
-            },
-            {
-                id: 5,
-                fullName: "David Henry",
-                designation: "DotNet Developer",
-                gender: "male",
-                teamName: "TeamB"
-            },
-            {
-                id: 6,
-                fullName: "Sarah Blake",
-                designation: "SQL Server DBA",
-                gender: "female",
-                teamName: "TeamB"
-            },
-            {
-                id: 7,
-                fullName: "James Bennet",
-                designation: "Angular Developer",
-                gender: "male",
-                teamName: "TeamC"
-            },
-            {
-                id: 8,
-                fullName: "Jessica Faye",
-                designation: "API Developer",
-                gender: "female",
-                teamName: "TeamC"
-            },
-            {
-                id: 9,
-                fullName: "Lita Stone",
-                designation: "C++ Developer",
-                gender: "female",
-                teamName: "TeamC"
-            },
-            {
-                id: 10,
-                fullName: "Daniel Young",
-                designation: "Python Developer",
-                gender: "male",
-                teamName: "TeamD"
-            },
-            {
-                id: 11,
-                fullName: "Adrian Jacobs",
-                designation: "Vue Developer",
-                gender: "male",
-                teamName: "TeamD"
-            },
-            {
-                id: 12,
-                fullName: "Devin Monroe",
-                designation: "Graphic Designer",
-                gender: "male",
-                teamName: "TeamD"
-            }
-        ]
-    );
-
-    function handleTeamSelectionChange(event) {
-        setTeam(event.target.value);
-    }
-
-    function handleEmployeeCardClick(event) {
-        const transformedEmployees = employees.map((employee) => employee.id === parseInt(event.currentTarget.id)
-            ? (employee.teamName === selectedTeam) ? { ...employee, teamName: '' } : { ...employee, teamName: selectedTeam }
-            : employee);
-        setEmployees(transformedEmployees);
-    }
-
+const Employees = ({employees, selectedTeam, handleEmployeeCardClick, handleTeamSelectionChange}) => {
+    
     return (
         <main className="container">
             <div className="row justify-content-center mt-3 mb-3">
@@ -124,7 +21,7 @@ const Employees = () => {
                     <div className="card-collection">
                         {
                             employees.map((employee) => (
-                                <div id={employee.id} className={(employee.teamName === selectedTeam ? "card m-2 standout" : "card m-2")} style={{cursor: "pointer"}} onClick={handleEmployeeCardClick}>
+                                <div ket={employee.id} id={employee.id} className={(employee.teamName === selectedTeam ? "card m-2 standout" : "card m-2")} style={{cursor: "pointer"}} onClick={handleEmployeeCardClick}>
                                     
                                     {(employee.gender === "male") ? <img src={maleProfile} className="card-img-top" />
                                                                     : <img src={femaleProfile} className="card-img-top" />}
--- src/Footer.js
+++ src/Footer.js
@@ -1,7 +1,11 @@
 const Footer = () => {
     return (
-        <footer>
-            <h1>Footer</h1>
+        <footer className="container">
+            <div className="row justify-content-center mt-3 mb-4">
+                <div className="col-8">
+                    <h5>Team Member Allocation App - {new Date().getFullYear()}</h5>
+                </div>
+            </div>
         </footer>
     )
 }
